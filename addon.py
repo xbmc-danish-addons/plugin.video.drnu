9582ce79ce4f53842083bd58a5434e8c634f7f89
Tomas Groth <second@tgc.dk>
2017-12-06 21:43:03 +0100

    Add support for other plugins to start live tv.

--- addon.py
+++ addon.py
@@ -325,7 +325,30 @@ class DrDkTvAddon(object):
                 
         xbmcplugin.setResolvedUrl(HANDLE, video['Uri'] is not None, item)
 
-                
+    def playLiveTV(self, slug):
+        item = None
+        url = None
+        for channel in self.api.getLiveTV():
+            # If the channel has the right slug, play the channel
+            if channel['Slug'] == slug:
+                server = None
+                for streamingServer in channel['StreamingServers']:
+                    if streamingServer['LinkType'] == 'HLS':
+                        server = streamingServer
+                        break
+                if server is None:
+                    continue
+
+                url = server['Server'] + '/' + server['Qualities'][0]['Streams'][0]['Stream']
+                item = xbmcgui.ListItem(channel['Title'], iconImage=channel['PrimaryImageUri'], path=url)
+                item.setProperty('Fanart_Image', channel['PrimaryImageUri'])
+                item.addContextMenuItems(self.menuItems, False)
+                break
+        if item:
+            xbmcplugin.setResolvedUrl(HANDLE, True, item)
+        else:
+            self.displayError(ADDON.getLocalizedString(30905) + ' ' + slug)
+
     def parseDate(self, dateString):
         if dateString is not None:
             try:
@@ -425,6 +448,9 @@ if __name__ == '__main__':
 
         elif 'playVideo' in PARAMS:
             drDkTvAddon.playVideo(PARAMS['playVideo'][0])
+
+        elif 'playLiveTV' in PARAMS:
+            drDkTvAddon.playLiveTV(PARAMS['playLiveTV'][0])
 
         elif 'addfavorite' in PARAMS:
             drDkTvAddon.addFavorite(PARAMS['addfavorite'][0])
