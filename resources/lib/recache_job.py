2fee9460c3dfbec689625e87a446ed402586c3de
termehansen <termopeten@gmail.com>
2022-04-01 17:15:17 +0200

    refactor

--- resources/lib/recache_job.py
+++ resources/lib/recache_job.py
@@ -1,17 +1,16 @@
 from tvapi import Api
-
 import os
-
-import xbmc
 import xbmcaddon
 import xbmcgui
 
 from xbmcvfs import translatePath
 
+
 def tr(id):
     if isinstance(id, list):
         return '\n'.join([addon.getLocalizedString(item) for item in id])
     return addon.getLocalizedString(id)
+
 
 addon = xbmcaddon.Addon('plugin.video.drnu')
 
@@ -21,10 +20,11 @@ if not os.path.exists(cachepath):
 
 api = Api(cachepath, tr)
 progress = xbmcgui.DialogProgress()
-progress.create("video.drnu recaching...")
+progress.create("video.drnu re-caching")
 progress.update(0)
 
-used_time = api.recache_requests(cache_urls=False, cache_episodes=True, clear_expired=False, verbose=True, progress=progress)
+used_time = api.recache_requests(cache_urls=False, cache_episodes=True,
+                                 clear_expired=False, verbose=True, progress=progress)
 
 progress.update(100)
 progress.close()
