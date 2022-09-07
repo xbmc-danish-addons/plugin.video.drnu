933487f26c4079e6cd9b4e85341ea1306e23c1ff
Termo <termopeten@gmail.com>
2022-09-07 21:36:23 +0200

    removed msj33

--- resources/lib/tvapi.py
+++ resources/lib/tvapi.py
@@ -1,5 +1,5 @@
 #
-#      Copyright (C) 2014 Tommy Winther, msj33, TermeHansen
+#      Copyright (C) 2014 Tommy Winther, TermeHansen
 #
 #  https://github.com/xbmc-danish-addons/plugin.video.drnu
 #
