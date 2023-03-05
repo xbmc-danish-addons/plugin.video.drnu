d6ef13c345ab634f6b5f0fe06a659e9aba6e5bfa
TermeHansen <termopeten@gmail.com>
2023-03-05 23:06:32 +0100

    request-cache braking change in 1.0.0

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -97,7 +97,7 @@ class TestOffline(unittest.TestCase):
         addon.addon.settings['disable.kids.seasons'] = 'true'
         handle.route(bluey['url'])
         episodes = [iteminfo(item) for item in get_items().values()]
-        self.myEqual(len(episodes), 29)
+        self.myEqual(len(episodes), 12)
 
         addon.addon.settings['disable.kids.seasons'] = 'false'
         handle.route(bluey['url'])
@@ -114,9 +114,9 @@ class TestOffline(unittest.TestCase):
 
         handle.route(a_aa[0]['url'])
         a = [iteminfo(item) for item in get_items().values()]
-        self.myEqual(len(a), 102)
+        self.myEqual(len(a), 96)
 
-        self.myEqual(a[0]['label'], 'A Very British Scandal')
+        self.myEqual(a[0]['label'], 'A Very English Scandal')
         handle.route(a[0]['url'])
 
     def test_search(self):
