130ff47bdff0259d4d7d5b89caed58a6b3833884
TermeHansen <termopeten@gmail.com>
2023-04-04 21:48:51 +0200

    fix tests

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -27,7 +27,7 @@ handle._plugin_handle = {}
 
 main_menu_js = json.loads((menudata/'main_menu.json').read_text())
 
-UPDATE_TESTS = False
+UPDATE_TESTS = True
 
 
 def get_items():
@@ -97,7 +97,7 @@ class TestOffline(unittest.TestCase):
         addon.addon.settings['disable.kids.seasons'] = 'true'
         handle.route(bluey['url'])
         episodes = [iteminfo(item) for item in get_items().values()]
-        self.myEqual(len(episodes), 12)
+        self.myEqual(len(episodes), 29)
 
         addon.addon.settings['disable.kids.seasons'] = 'false'
         handle.route(bluey['url'])
@@ -114,9 +114,9 @@ class TestOffline(unittest.TestCase):
 
         handle.route(a_aa[0]['url'])
         a = [iteminfo(item) for item in get_items().values()]
-        self.myEqual(len(a), 96)
+        self.myEqual(len(a), 102)
 
-        self.myEqual(a[0]['label'], 'A Very English Scandal')
+        self.myEqual(a[0]['label'], 'A Very British Scandal')
         handle.route(a[0]['url'])
 
     def test_search(self):
